443d56d51c93c6b65f1c4055787969ae1f7c0402
Victor-Pizzaia <v_pizzaia@hotmail.com>
2024-02-15 16:07:50 -0300

    initial commit

---
+++ src/main/java/br/com/web/odonto/WebOdontoApplication.java
@@ -0,0 +1,13 @@
+package br.com.web.odonto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOdontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOdontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/web/odonto/WebOdontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.web.odonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebOdontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
